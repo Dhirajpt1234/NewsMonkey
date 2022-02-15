b0dc9b6457efa135c2dc5aaa36afb4b9f0a5a535
Dhiraj <dpthorat85@gmail.com>
2022-02-15 22:29:34 +0530

    created navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <h1>
+          hiiiii bro you are going to start a new journey with react class based Component
+        </h1>
+        
+      </div>
+    )
+  }
+}
+
+export default App
